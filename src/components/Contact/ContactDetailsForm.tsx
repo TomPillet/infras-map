b5d0e6a1836915afea658278f152d4adb2a209c9
Tom <tom963741@gmail.com>
2023-04-17 13:26:29 +0200

    feat: add css to save button

--- src/components/Contact/ContactDetailsForm.tsx
+++ src/components/Contact/ContactDetailsForm.tsx
@@ -54,7 +54,7 @@ const ContactDetailsForm: FC<ContactDetailsFormProps> = ({contact, containerId, 
         <input key={contact.key+'-lng'} type="number" onChange={handleChange} className={"w-1/4 " + defaultInputStyle} name="longitude" defaultValue={contact.longitude} disabled={!isEditable} required />
       </div>
 
-      <button type="button" onClick={() => {
+      <button className='block m-auto mt-4 px-8 py-2 bg-green-500 text-white rounded hover:font-bold' type="button" onClick={() => {
         if (contact.name.length > 0) {
           document.getElementById(containerId)!.classList.add("hidden");
           handleSave(contact);
